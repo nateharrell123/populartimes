e31fe3f221b01392e97ebb1971353eb9c1bae268
Nate Harrell <nateharrell@ksu.edu>
2021-09-21 09:08:26 -0500

    writing script

--- scripts/Popular_Times.py
+++ scripts/Popular_Times.py
@@ -1,3 +1,9 @@
 import populartimes
+import json
 
-print("hello!");
+spot = populartimes.get_id("AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo", "ChIJL2TntXnSvYcRvdwEv_SZehA")
+
+for key, value in spot.items():
+    if key == "current_popularity":
+        print(value)
+        
