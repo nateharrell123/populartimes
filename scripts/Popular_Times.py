c14fbf6e50dc57d42155526588bce64e278f77aa
Nate Harrell <nate.harrell@me.com>
2021-11-11 16:25:29 -0600

    wow

--- scripts/Popular_Times.py
+++ scripts/Popular_Times.py
@@ -14,10 +14,11 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/test', methods=['GET'])
 def GetBusyness():
-    spot = populartimes.get_id("AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo", "ChIJL2TntXnSvYcRvdwEv_SZehA")
-    for key, value in spot.items():
-        if key == "current_popularity":
-            print(value)
+    # spot = populartimes.get_id("AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo", "ChIJL2TntXnSvYcRvdwEv_SZehA")
+    # for key, value in spot.items():
+    #     if key == "current_popularity":
+    #         print(value)
+    return 'this works!'
 
 
 # idk 
