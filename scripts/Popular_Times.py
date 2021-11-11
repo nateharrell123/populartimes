6c7a403f36afd19e0f7b77b6e916325451844405
Nate Harrell <nate.harrell@me.com>
2021-11-11 16:19:59 -0600

    setup

--- scripts/Popular_Times.py
+++ scripts/Popular_Times.py
@@ -1,9 +1,25 @@
 import populartimes
 import json
-import flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-spot = populartimes.get_id("AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo", "ChIJL2TntXnSvYcRvdwEv_SZehA")
 
-for key, value in spot.items():
-    if key == "current_popularity":
-        print(value)
+# Instantiate app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Allow Cors access
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+@app.route('/test', methods=['GET'])
+def GetBusyness():
+    spot = populartimes.get_id("AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo", "ChIJL2TntXnSvYcRvdwEv_SZehA")
+    for key, value in spot.items():
+        if key == "current_popularity":
+            print(value)
+
+
+# idk 
+if __name__ == '__main__':
+    app.run()
