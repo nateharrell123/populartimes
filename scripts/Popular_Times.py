fe82c8cc69dbcedb38dcfd953790e54b8527e6a4
Nate Harrell <nate.harrell@me.com>
2021-11-11 11:18:46 -0600

    add stuff

--- scripts/Popular_Times.py
+++ scripts/Popular_Times.py
@@ -1,9 +1,9 @@
 import populartimes
 import json
 
+
 spot = populartimes.get_id("AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo", "ChIJL2TntXnSvYcRvdwEv_SZehA")
 
 for key, value in spot.items():
     if key == "current_popularity":
         print(value)
-        
