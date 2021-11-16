9468342917b8a90a44c902b9771d17497b9c0f45
Nate Harrell <nate.harrell@me.com>
2021-11-16 12:54:56 -0600

    backend done

--- scripts/Popular_Times.py
+++ scripts/Popular_Times.py
@@ -25,49 +25,27 @@ def GetBusyness():
     for key, value in spot.items():
         if key == "current_popularity":
             if value > 0 and value < 25:
-                print(value) # Not Very Busy 
+                return "Not very busy!"
             elif value > 25 and value < 50:
-                print(value) # A little busy
+                return "A little busy"
             elif value > 50 and value < 75: 
-                print(value) # Moderately busy
+                return "Moderately busy"
             elif value > 75:
-                print(value) # Very busy
+                return "Very busy"
         if key == "populartimes":
             for item, val in value[dayOfWeek].items():
                 if item == "data":
-                    print(val[currentTime])
+                    return val[currentTime]
         else:
-            return 'No population data for this establishment.'
+            return "No population data for this establishment."
 
 
 # idk 
 if __name__ == '__main__':
     app.run()
 
-
-# dayOfWeek = calendar.day_name[my_date.weekday()] English
-#print(dayOfWeek)
-
-
 # Dirty Dawg's (not live)
-notLive = populartimes.get_id("AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo", "ChIJOWMYXHjSvYcRSvAfdxlP8fg")
-
-for key, value in notLive.items():
-
-
+#notLive = populartimes.get_id("AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo", "ChIJOWMYXHjSvYcRSvAfdxlP8fg")
 
 # Walmart Manhattan, KS (live busyness)
 # spot = populartimes.get_id("AIzaSyDASvg4ATeMQcAsocmem5kFdTMDw_NSJwo", "ChIJqa09skLSvYcR47QwBQ73ShQ")
-
-for key, value in spot.items():
-    if key == "current_popularity":
-        if value > 0 and value < 25:
-            print(value) # Not Very Busy 
-        elif value > 25 and value < 50:
-            print(value) # A little busy
-        elif value > 50 and value < 75: 
-            print(value) # Moderately busy
-        elif value > 75:
-            print(value) # Very busy
-    elif key == "populartimes":
-        print("need to do this")
